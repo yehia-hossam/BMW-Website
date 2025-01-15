f0d5df1df532b39a67686303f4f570b1465c6b0e
Yehia Hossam <157986214+yehia-hossam@users.noreply.github.com>
2025-01-15 06:42:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('container');
+
+
+function cars (car){
+    img.src = car;
+}
+
+function colors(color){
+    container.style.backgroud =color
+}
